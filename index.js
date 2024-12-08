cffc91e15d0638fa53d8448a9c0abaeeba419428
Surendra Dhaka <saabchoudhary350@gmail.com>
2024-12-08 08:47:07 +0530

    Q & A

---
+++ index.js
@@ -0,0 +1,9 @@
+let questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  let btn = question.querySelector(".question-btn");
+
+  btn.addEventListener("click", function () {
+    question.classList.toggle("show-text");
+  });
+});
